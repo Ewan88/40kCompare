3579c93c135fc61f1709b5d0ace987ffc6c21685
Ewan <baileyewan@gmail.com>
2024-03-16 19:18:19 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { defineComponent } from 'vue';
+	import WeaponCard from './components/WeaponCard.vue';
+
+	export default defineComponent({
+		name: 'App',
+		components: {
+			WeaponCard,
+		},
+	});
+</script>
+
+<template>
+	<WeaponCard />
+</template>
---
+++ src/components/WeaponCard.vue
@@ -0,0 +1,44 @@
+<template>
+	<table>
+		<tr>
+			<th>Weapon</th>
+			<th>Range</th>
+			<th>Attacks</th>
+			<th>Ballistic Skill</th>
+			<th>Strength</th>
+			<th>Armour Piercing</th>
+			<th>Damage</th>
+			<th>Abilities</th>
+		</tr>
+		<tr>
+			<td><input type="text" v-model="input1" /></td>
+			<td><input type="number" v-model="input2" /></td>
+			<td><input type="number" v-model="input3" /></td>
+			<td><input type="number" v-model="input4" /></td>
+			<td><input type="number" v-model="input5" /></td>
+			<td><input type="number" v-model="input6" /></td>
+			<td><input type="number" v-model="input7" /></td>
+			<td><input type="text" v-model="input8" /></td>
+		</tr>
+	</table>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+
+	export default defineComponent({
+		name: 'WeaponCard',
+		data() {
+			return {
+				input1: '',
+				input2: 0,
+				input3: 0,
+				input4: 0,
+				input5: 0,
+				input6: 0,
+				input7: 0,
+				input8: '',
+			};
+		},
+	});
+</script>
